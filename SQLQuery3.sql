a04571b3361cb84752a31c2876c6447363855d85
samuel-soto2 <1461888@senati.pe>
2023-09-14 12:37:20 -0500

    tenemos ocho permisos de los cuales asignamos a quien darle o no los permisos corespondientes dependiendo si es administrador o usuario

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -40,3 +40,39 @@ insert into Roles values ('User')
 
 select * from Roles
 
+insert into Permisos values ('Create')
+insert into Permisos values ('Read')
+insert into Permisos values ('Update')
+insert into Permisos values ('Delete')
+
+select * from Permisos
+
+select * from Roles_Permisos
+
+insert into Roles_Permisos values (1,1,1)
+insert into Roles_Permisos values (1,2,1)
+insert into Roles_Permisos values (1,3,1)
+insert into Roles_Permisos values (1,4,1)
+
+insert into Roles_Permisos values (2,1,0)
+insert into Roles_Permisos values (2,2,1)
+insert into Roles_Permisos values (2,3,0)
+insert into Roles_Permisos values (2,4,0)
+
+create procedure sp_registrar
+@Nombre varchar(50),
+@Apellido varchar(50),
+@Correo varchar(70),
+@Edad int,
+@Usuario varchar(50),
+@Contraseña varchar(max),
+@Cod_Rol int,
+@Patron varchar (50)
+as begin
+insert  into Usuarios values (@Nombre , @Apellido , @Correo, @Edad , @Usuario , ENCRYPTBYPASSPHRASE (@Patron , @Contraseña) , @Cod_Rol)
+end
+
+
+
+
+
