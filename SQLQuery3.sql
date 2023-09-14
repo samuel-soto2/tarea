eb4cc43024aefa938433552885110c974a051a3d
samuel-soto2 <1461888@senati.pe>
2023-09-14 11:52:08 -0500

    aqui tenemos sql donde le vamos a crear con la intencion de poder establecer una conexion

---
+++ SQLQuery3.sql
@@ -0,0 +1,12 @@
+﻿﻿CREATE DATABASE Portalweb
+USE Portalweb
+
+CREATE TABLE Usuarios(
+Cod_Usuario int identity(1,1),
+Nombre varchar(50),
+Apellido varchar(50),
+Correo varchar(70),
+Edad int,
+Usuario varchar(50),
+Contraseña varchar(50)
+)
